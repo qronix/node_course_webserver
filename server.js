07b8ec233c32b5588724f2e733398ddc4e9baaf4
qronix <qroniix@gmail.com>
2018-09-25 13:49:18 -0400

    Prepped for Heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs  = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -20,11 +21,11 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use((req,res,next)=>{
-    res.render('maintenance.hbs',{
-        maintMsg: 'The site will be back soon!'
-    });
-});
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs',{
+//         maintMsg: 'The site will be back soon!'
+//     });
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -55,6 +56,6 @@ app.get('/bad',(req,res)=>{
     });
 });
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
 });
